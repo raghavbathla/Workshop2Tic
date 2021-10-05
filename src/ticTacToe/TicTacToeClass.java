365fd611091ed13e2c183248ae968128072b5a7e
DELL <bathla.raghav5@gmail.com>
2021-10-05 15:01:15 +0530

    Add UC2(ask user)

--- src/ticTacToe/TicTacToeClass.java
+++ src/ticTacToe/TicTacToeClass.java
@@ -1,16 +1,36 @@
 package ticTacToe;
 
-public class TicTacToeClass {
-    static private char  board[] = new char[10];
+import java.security.cert.X509Certificate;
+import java.util.Scanner;
 
-    static private void assign(){
+public class TicTacToeClass {
+
+
+    static private char[] assign(){
+         char  board[] = new char[10];
         for(int i=1; i <=board.length-1; i++){
             board[i] = ' ';
         }
+        return board;
     }
+
+    static private char askUser(){
+        Scanner scanner  = new Scanner(System.in);
+    System.out.println("Choose between X or O");
+
+    System.out.println("Enter 1 for X or 2 for O");
+    int val  = scanner.nextInt();
+    if(val == 1)
+        return 'X';
+    else
+        return 'Y';
+
+    }
+
 
     public static void main(String[] args)
     {
         assign();
+        askUser();
     }
 }
